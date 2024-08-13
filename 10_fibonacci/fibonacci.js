3c56b5af03e20f7c9688085d5bf354a2b00169a4
Talha <talhahussain.splitarts@gmail.com>
2024-08-13 22:03:01 +0500

    Completed All Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  count = 0;
+  if (Number.isNaN(num) || num < 0) {
+    return false;
+  }
+  
+  let firstPrev = 1;
+  let secondPrev = 0;
 
-};
+  for (let i = 2; i <= num; i++) {
+      let current = firstPrev + secondPrev;
+      secondPrev = firstPrev;
+      firstPrev = current;
+  }
+
+  return firstPrev;
+}
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(value => value.title);
 };
 
+
+  
 // Do not edit below this line
 module.exports = getTheTitles;
